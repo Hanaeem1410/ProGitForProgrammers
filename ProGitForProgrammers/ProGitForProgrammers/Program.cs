068d5e90d996b4fbc649e85e53cf0e7b3ee3df70
2101207 <2101207@MC12257>
2024-10-23 14:53:14 +0100

    Created new project in Visual Studio

---
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProGitForProgrammers
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+      }
+}
